17c545d545aae1b9cabb05736d640e09bd486320
ryan910707 <74304456+ryan910707@users.noreply.github.com>
2022-10-23 13:00:15 +0800

    Add files via upload

---
+++ image_labeler/package/winker.py
@@ -0,0 +1,65 @@
+# import the necessary packages
+from scipy.spatial import distance as dist
+from imutils import face_utils
+import numpy as np
+import imutils
+import dlib
+import cv2
+
+
+def eye_aspect_ratio(eye):
+    # compute the euclidean distances between the two sets of
+    # vertical eye landmarks (x, y)-coordinates
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    # compute the euclidean distance between the horizontal
+    # eye landmark (x, y)-coordinates
+    C = dist.euclidean(eye[0], eye[3])
+    # compute the eye aspect ratio
+    ear = (A + B) / (2.0 * C)
+    # return the eye aspect ratio
+    return ear
+
+
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+# grab the indexes of the facial landmarks for the left and
+# right eye, respectively
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+
+def wink_detection(path):
+    image = cv2.imread(path)
+    image = imutils.resize(image, width=450)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    rects = detector(gray, 0)
+    for rect in rects:
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+        # extract the left and right eye coordinates, then use the
+        # coordinates to compute the eye aspect ratio for both eyes
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+        # average the eye aspect ratio together for both eyes
+        ear = (leftEAR + rightEAR) / 2.0
+        # compute the convex hull for the left and right eye, then
+        # visualize each of the eyes
+        # leftEyeHull = cv2.convexHull(leftEye)
+        # rightEyeHull = cv2.convexHull(rightEye)
+        # cv2.drawContours(image, [leftEyeHull], -1, (0, 255, 0), 1)
+        # cv2.drawContours(image, [rightEyeHull], -1, (0, 255, 0), 1)
+        # check to see if the eye aspect ratio is below the blink
+        # threshold, and if so, increment the blink frame counter
+        if ear < 0.2:
+            print("ㄆㄧㄚ了")
+            return 1
+        else:
+            print("正常")
+            return 0
